9436f1240d662535b92b3cc200a2aa48a5b23f1d
Andrew Coleburn <arcoleburn@gmail.com>
2020-12-20 15:33:45 -0500

    edited postgrator

--- postgrator-config.js
+++ postgrator-config.js
@@ -4,6 +4,10 @@ require('dotenv').config();
 module.exports = {
   migrationDirectory: 'migrations',
   driver: 'pg',
+  connectionString:
+    process.env.NODE_ENV === 'test'
+      ? process.env.TEST_DB_URL
+      : process.env.DATABASE_URL,
   host: process.env.MIGRATION_DB_HOST,
   port: process.env.MIGRATION_DB_PORT,
   database: process.env.MIGRATION_DB_NAME,
