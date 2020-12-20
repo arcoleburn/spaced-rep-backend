1f690e266a682085a865967f7fed2b144f075bc1
Andrew Coleburn <arcoleburn@gmail.com>
2020-12-20 17:07:59 -0500

    db vars

--- postgrator-config.js
+++ postgrator-config.js
@@ -8,9 +8,9 @@ module.exports = {
     process.env.NODE_ENV === 'test'
       ? process.env.TEST_DB_URL
       : process.env.DATABASE_URL,
-  host: process.env.MIGRATION_DB_HOST,
-  port: process.env.MIGRATION_DB_PORT,
-  database: process.env.MIGRATION_DB_NAME,
-  username: process.env.MIGRATION_DB_USER,
-  password: process.env.MIGRATION_DB_PASS,
+  // host: process.env.MIGRATION_DB_HOST,
+  // port: process.env.MIGRATION_DB_PORT,
+  // database: process.env.MIGRATION_DB_NAME,
+  // username: process.env.MIGRATION_DB_USER,
+  // password: process.env.MIGRATION_DB_PASS,
 };
