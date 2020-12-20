e3bf90ad7fce4e14a2b39c0b776367f39bfe621d
Andrew Coleburn <arcoleburn@gmail.com>
2020-12-20 16:56:18 -0500

    changed error handling displays

--- src/middleware/error-handler.js
+++ src/middleware/error-handler.js
@@ -2,7 +2,7 @@ const { NODE_ENV } = require('../config')
 
 module.exports = function errorHandler(error, req, res, next) {
   const response = (NODE_ENV === 'production')
-    ? { error: 'Server error' }
+    ? { error: error}
     : (console.error(error), { error: error.message, details: error })
 
   res.status(500).json(response)
